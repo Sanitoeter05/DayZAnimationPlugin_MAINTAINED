86ade2c53afca8f077661e98a9313c73a95a6d05
Sanitoeter05 <75578838+Sanitoeter05@users.noreply.github.com>
2026-06-24 19:58:57 +0200

    Refactored INIT scripts (Registration and unregistration)

---
+++ BlenderPlugin/DayzAnimationTools/modules/bpyHandler.py
@@ -0,0 +1,16 @@
+import bpy
+def registerClasses(classes):
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def registerMenus(menus, menuClass):
+    for menu in menus:
+        getattr(bpy.types, menuClass).append(menu)
+
+def unregisterClasses(classes):
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+def unregisterMenus(menus, menuClass):
+    for menu in menus:
+        getattr(bpy.types, menuClass).remove(menu)
